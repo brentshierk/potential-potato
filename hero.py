b442054be99eddae279a20f1322ba4ddc57399fa
brentshierk <34776645+brentshierk@users.noreply.github.com>
2020-11-19 10:44:03 -0500

    work in progress pt4

--- hero.py
+++ hero.py
@@ -1,20 +1,50 @@
 import random
+from Ability import Ability
+from Armor import Armor
 class Hero:
     def __init__(self,name,starting_health=100):
         self.name = name
         self.starting_health = 100
         self.current_health = starting_health
+        self.abilities = list()
+        self.armours = list()
 
 
-    def fight(self,opponent):
-        winner = random.choice([hero1,opponent])
+    def fight(self,hero,opponent):
+        winner = random.choice([hero,opponent])
         print(winner.name,'has won!')
-        
+
+    def add_ability(self,ability):
+        self.abilities.append(ability)
+    
+    def add_armor(self,armor):
+        self.armours.append(armor)
+
+
+    def attack(self):
+        total_damgae = 0
+
+        for ability in self.abilities:
+            total_damgae += ability.attack()
+        return total_damgae
+    
+    def defend(self,damage_amt):
+        damage_amt = 0
+
+        for armor in self.armours:
+            damage_amt += armor.defend()
+        return damage_amt
+
+
+
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    hero1 = Hero("Grace Hopper",200)
-    hero2 = Hero('Brent Shierk')
-    hero1.fight(hero2)
+    # this block of code is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
